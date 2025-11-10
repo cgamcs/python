4d267aaf3636328152b8c8fbc5e544a69c592395
cgamcs <cesar.garciamcs@proton.me>
2025-11-10 15:55:02 -0600

    Agregando dia 5

---
+++ Dia 5/Ayuda.py
@@ -0,0 +1,18 @@
+dic = {'clave1': 100, 'clave2': 200}
+elemento_eliminado = dic.popitem()
+print(elemento_eliminado)
+print(dic)
+
+print('\n')
+print(",:_#,,,,,,:::____##Pyt%on_ _Total,,,,,,::#".lstrip(',:%_#'))
+
+print('\n')
+frutas = ["mango", "banana", "cereza", "ciruela", "pomelo"]
+frutas.insert(3, 'naranja')
+print(frutas)
+
+print('\n')
+marcas_smartphones = {"Samsung", "Xiaomi", "Apple", "Huawei", "LG"}
+marcas_tv = {"Sony", "Philips", "Samsung", "LG"}
+conjuntos_aislados = marcas_smartphones.isdisjoint(marcas_tv)
+print(conjuntos_aislados)
